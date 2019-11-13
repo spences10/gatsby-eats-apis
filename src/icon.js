429966e318b74d7b2eecfac91a2034a46fd68f2b
spences10 <spences10apps@gmail.com>
2019-11-13 16:10:52 +0000

    postitioning things

--- src/icon.js
+++ src/icon.js
@@ -2,12 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 
 const IconDiv = styled.div`
-  height: 15rem;
-  width: 15rem;
-  position: relative;
-  left: 50%;
-  margin-top: -30px;
-  margin-left: -30px;
+  height: 20rem;
+  width: 20rem;
   background-image: url(${({ url }) => url});
   background-repeat: no-repeat;
   background-size: cover;
